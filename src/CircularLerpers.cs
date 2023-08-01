aa6e0073fc68886c98be3731cd1b54ba285743ae
microlith57 <microlith57@gmail.com>
2023-08-01 12:24:31 +1200

    rework to use multiple lists; add easer support

--- src/CircularLerpers.cs
+++ src/CircularLerpers.cs
@@ -6,53 +6,72 @@ using System.Linq;
 
 namespace Celeste.Mod.RainTools {
     public abstract class CircularLerper<T, T_out> {
-        public List<KeyValuePair<float, T>> Stops;
-        public bool Any => Stops.Count > 0;
+        public List<float> StopAngles;
+        public List<T> StopValues;
+        public List<Ease.Easer> StopEasers;
+
+        public int Count => StopAngles.Count;
+        public bool Any => StopAngles.Count > 0;
 
         public CircularLerper() {
-            Stops = new();
+            StopAngles = new();
+            StopValues = new();
+            StopEasers = new();
         }
 
-        public CircularLerper(IEnumerable<KeyValuePair<float, T>> stops) : this() {
-            foreach (var kvp in stops)
-                Add(kvp);
+        public virtual void Add(float angleRadians, T stopValue, Ease.Easer ease) {
+            if (Count == 0 || angleRadians > StopAngles.Last()) {
+                StopAngles.Add(angleRadians);
+                StopValues.Add(stopValue);
+                StopEasers.Add(ease);
+                return;
+            }
+            if (angleRadians < StopAngles.First()) {
+                StopAngles.Insert(0, angleRadians);
+                StopValues.Insert(0, stopValue);
+                StopEasers.Insert(0, ease);
+                return;
+            }
+
+            int index = StopAngles.BinarySearch(angleRadians);
+            if (index < 0)
+                index = ~index;
+
+            StopAngles.Insert(index, angleRadians);
+            StopValues.Insert(index, stopValue);
+            StopEasers.Insert(index, ease);
         }
 
-        public void Add(KeyValuePair<float, T> kvp) {
-            Stops.AddSorted(kvp, new KVPComparer<float, T>());
-        }
-
-        public virtual void Add(float angleRadians, T stop) {
-            Add(new(Calc.WrapAngle(angleRadians), stop));
+        public void Add(float angleRadians, T stop) {
+            Add(angleRadians, stop, Ease.Linear);
         }
 
         public abstract T_out Convert(T val);
         public abstract T_out Lerp(T a, T b, float fac);
 
         public T_out GetOrDefault(float angleRadians) {
-            if (Stops.Count == 0) {
+            if (Count == 0) {
                 return default(T_out);
-            } else if (Stops.Count == 1) {
-                return Convert(Stops.First().Value);
+            } else if (Count == 1) {
+                return Convert(StopValues.First());
             } else {
-                int next_index = Stops.BinarySearch(new(angleRadians, default), new KVPComparer<float, T>());
+                int next_index = StopAngles.BinarySearch(angleRadians);
                 if (next_index < 0)
                     next_index = ~next_index;
-                if (next_index >= Stops.Count)
+                if (next_index >= Count)
                     next_index = 0;
 
                 int prev_index = next_index - 1;
                 if (prev_index < 0)
-                    prev_index = Stops.Count - 1;
+                    prev_index = Count - 1;
 
-                var a = Stops[prev_index];
-                var b = Stops[next_index];
+                var ease = StopEasers[prev_index];
 
-                float dist_a = Calc.AbsAngleDiff(a.Key, angleRadians);
-                float dist_b = Calc.AbsAngleDiff(b.Key, angleRadians);
-                float fac = Calc.Clamp((dist_a) / (dist_a + dist_b), 0f, 1f);
+                float dist_a = Calc.AbsAngleDiff(StopAngles[prev_index], angleRadians);
+                float dist_b = Calc.AbsAngleDiff(StopAngles[next_index], angleRadians);
+                float fac = ease(dist_a / (dist_a + dist_b));
 
-                return Lerp(a.Value, b.Value, fac);
+                return Lerp(StopValues[prev_index], StopValues[next_index], fac);
             }
         }
 
@@ -108,14 +127,14 @@ namespace Celeste.Mod.RainTools {
     public class CircularColorGradientLerper : SimpleCircularLerper<Color[]> {
         public int Length { get; private set; }
 
-        public override void Add(float angleRadians, Color[] stop) {
-            if (Stops.Count == 0)
+        public override void Add(float angleRadians, Color[] stop, Ease.Easer easer) {
+            if (Count == 0)
                 Length = stop.Length;
 
             if (stop.Length != Length)
                 throw new ArgumentException("gradient stop is of incorrect length for this gradient");
 
-            base.Add(angleRadians, stop);
+            base.Add(angleRadians, stop, easer);
         }
 
         public override Color[] Lerp(Color[] a, Color[] b, float fac) {
