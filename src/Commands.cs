33e4421479386b8270ee3c7e96bc40ac7686a1eb
microlith57 <microlith57@gmail.com>
2023-08-05 16:39:50 +1200

    more concise commands

--- src/Commands.cs
+++ src/Commands.cs
@@ -3,18 +3,18 @@ using Monocle;
 namespace Celeste.Mod.RainTools {
     public static class Commands {
 
-        [Command("cycle_set", "set a cycle progression to the given value")]
+        [Command("cycleset", "set a cycle progression to the given value")]
         public static void CycleSet(string cycle, float value = 0) {
             Cycles.SetProgression(cycle, value);
         }
 
-        [Command("cycle_get", "get a cycle progression and angle")]
+        [Command("cycleget", "get a cycle progression and angle")]
         public static void CycleGet(string cycle) {
             Engine.Commands.Log($"progression: {Cycles.GetProgression(cycle)}");
             Engine.Commands.Log($"angle: {Cycles.GetAngle(cycle)}");
         }
 
-        [Command("cycle_debug", "show a given cycle's debugging visualisation")]
+        [Command("cycledbg", "show a given cycle's debugging visualisation")]
         public static void CycleSet(string cycle) {
             if (cycle == "")
                 Cycles.Debugging = null;
