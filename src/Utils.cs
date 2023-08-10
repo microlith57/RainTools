f32e8e638b0ae0778c555c939dc57b0ef7e77821
microlith57 <microlith57@gmail.com>
2023-08-10 16:00:50 +1200

    add blackbody approximation

--- src/Utils.cs
+++ src/Utils.cs
@@ -1,6 +1,9 @@
 
 using Microsoft.Xna.Framework;
+using Monocle;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Celeste.Mod.RainTools {
     internal static class Utils {
@@ -36,6 +39,39 @@ namespace Celeste.Mod.RainTools {
 
         internal static Vector2 Proj(this Vector2 vec, Vector2 onto) {
             return Vector2.Dot(vec, onto) / Vector2.Dot(onto, onto) * onto;
+        }
+
+        private static Dictionary<float, Color> BlackbodyColors = new() {
+            {500, new(0, 0, 0)},
+            {550, new(53, 34, 1)},
+            {630, new(84, 40, 3)},
+            {680, new(104, 17, 0)},
+            {740, new(134, 22, 0)},
+            {780, new(160, 0, 0)},
+            {810, new(193, 27, 27)},
+            {850, new(212, 65, 21)},
+            {900, new(233, 88, 44)},
+            {950, new(233, 126, 28)},
+            {1000, new(255, 170, 15)},
+            {1100, new(251, 192, 52)},
+            {1200, new(255, 207, 97)},
+            {1300, new(255, 230, 173)},
+            {2000, new(255, 255, 255)},
+        };
+        private static float MinBlackbodyTemp = BlackbodyColors.Keys.Min();
+        private static float MaxBlackbodyTemp = BlackbodyColors.Keys.Max();
+
+        public static Color Blackbody(float celsius) {
+            if (celsius <= MinBlackbodyTemp)
+                return BlackbodyColors[MinBlackbodyTemp];
+            if (celsius >= MaxBlackbodyTemp)
+                return BlackbodyColors[MaxBlackbodyTemp];
+
+            var below = BlackbodyColors.Keys.Where(t => t <= celsius).Max();
+            var above = BlackbodyColors.Keys.Where(t => t > celsius).Min();
+            var fac = Calc.ClampedMap(celsius, below, above, 0, 1);
+
+            return Color.Lerp(BlackbodyColors[below], BlackbodyColors[above], fac);
         }
 
     }
