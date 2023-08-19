945032ab2a070e8b883d15ddb5d76fa8ea99e423
microlith57 <microlith57@gmail.com>
2023-08-20 00:36:51 +1200

    add displacement parallax styleground

--- src/Backdrops/IHasAngle.cs
+++ src/Backdrops/IHasAngle.cs
@@ -1,10 +1,3 @@
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using Monocle;
-using System.Linq;
-using Celeste.Mod.Backdrops;
-using System;
-
 namespace Celeste.Mod.RainTools.Backdrops {
     internal interface IHasAngle {
         float GetAngle();
